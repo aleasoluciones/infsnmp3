ff800a06f5cd160de5b6c037ce5224443703875b
Jorge Senín <jsenin@alea-soluciones.com>
2019-05-24 08:35:41 +0200

    [INTERNAL] Add requirement dependencies to setup.py

--- setup.py
+++ setup.py
@@ -6,4 +6,6 @@ setup(name='infsnmp',
       description='snmp library',
       platforms='Linux',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      install_requires=['pysnmp==4.2.4','pysnmp-mibs==0.1.4','infcommon'],
+      dependency_links=['git+https://github.com/aleasoluciones/infcommon3.git#egg=infcommon'],
       scripts=[])
