327e4e20cb661535e6cc0df5e7372d04ee7a0def
Luis de Dios <luisdedios@alea-soluciones.com>
2020-01-31 11:27:10 +0100

    [SYSTEMS] Closes #2 Move Docker-related files to dev and use Docker Compose

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.7-slim
-MAINTAINER bifer@alea-soluciones.com
-
-RUN pip install snmpsim==0.1.5
-
-EXPOSE 1161/udp
-
-CMD python $(which snmpsimd.py) --v2c-arch --agent-udpv4-endpoint=0.0.0.0:1161 --device-dir=/simulated_data/ --validate-device-data
--- dev/integration_tests.sh
+++ dev/integration_tests.sh
@@ -2,11 +2,9 @@
 set -e
 
 trap "docker stop -t 0 snmpsimd; docker rm snmpsimd; exit" SIGHUP SIGINT SIGTERM ERR
-docker build -t snmpsimd .
-docker run -d --name snmpsimd -v /etc/localtime:/etc/localtime:ro -v $(pwd)/integration_tests/snmpsim/simulated_data/:/simulated_data -p 1161:1161/udp snmpsimd
+docker-compose -f dev/infsnmp3_devdocker/docker-compose.yml up -d
 sleep 5
 nosetests $INTEGRATION_TESTS -s --logging-clear-handlers --processes=16 --process-timeout=50 --with-yanc
 NOSE_RETCODE=$?
-docker stop -t 0 snmpsimd
-docker rm snmpsimd
+docker-compose -f dev/infsnmp3_devdocker/docker-compose.yml down
 exit $NOSE_RETCODE
