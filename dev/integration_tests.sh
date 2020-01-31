a6b8a4020bcb2e45d72fde87e3471037e85405fc
Luis de Dios <luisdedios@alea-soluciones.com>
2020-01-31 15:11:56 +0100

    [FIX] Don't check by container's name

--- dev/integration_tests.sh
+++ dev/integration_tests.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-IS_SNMPSIM_CONTAINER_RUNNING=$(docker inspect -f '{{.State.Running}}' infsnmp3_devdocker_snmpsim_1)
-
-if [[ $IS_SNMPSIM_CONTAINER_RUNNING == "false" ]]; then
-    echo "Please start the dev dependencies before running the integration tests."
-    exit 1
-fi
-
 nosetests $INTEGRATION_TESTS -s --logging-clear-handlers --processes=16 --process-timeout=50 --with-yanc
 NOSE_RETCODE=$?
 
