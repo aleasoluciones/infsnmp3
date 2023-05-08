700ec597c7ae3fdc42800e0b0c67543a92871f04
Alberto <acruz@alea-soluciones.com>
2023-05-08 08:04:55 +0200

    [FIX] Pyasn1 to 0.4.8 version
    - Fix version due to backward incompatibility issues
      (https://github.com/pyasn1/pyasn1/issues/28)

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='infsnmp',
                         'pysnmp-pyasn1==1.1.2',
                         'pysnmp-pysmi==1.1.10',
                         'pysnmplib==5.0.21',
+                        'pyasn1==0.4.8',
                         'infcommon'],
       dependency_links=['git+https://github.com/aleasoluciones/infcommon3.git#egg=infcommon'],
       scripts=[])
